c19c85c8a6288624f9b36429bc4980e536c2d9cc
max-ionov <max.ionov@gmail.com>
2024-12-12 18:37:35 +0100

    API prototype

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# postime-dts
+# PosTime API
+
+An API required for the [frontend](https://github.com/max-ionov/postil-time-machine-viewer)
+
+## Project Setup
+
+It is recommended to use a virtual environment:
+
+```bash
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+To deactivate the virtual environment, use `deactivate` command
+
+## Starting the API for development
+
+```bash
+source .venv/bin/activate
+python app.py
+```
+
+## Starting the API for production
+
+A recommended way to use the API in production is via gunicorn, a WSGI server, ideally in combination with nginx.
+Specific deployment details will vary on the system and the toolchain used to deploy services, but in a nutshell the command that starts the server will be similar to
+
+```bash
+gunicorn wsgi:app --bind 0.0.0.0:8080 --workers 4
+```
+
---
+++ app.py
@@ -0,0 +1,154 @@
+import json
+import logging
+import os
+
+import requests
+import xml.etree.ElementTree as ET
+
+from flask import Flask, jsonify, abort
+from flask_cors import CORS
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+DATA_PATH = os.getenv("POSTIME_DATA", "data.json")
+
+HOST = os.getenv("POSTIME_DTS_HOST", "0.0.0.0")
+PORT = os.getenv("POSTIME_DTS_PORT", 8080)
+
+GH_USER = os.getenv("POSTIME_GH_USER", "postime")
+
+API_CLIENT = os.getenv("POSTIME_API_CLIENT", "http://localhost:5173")
+API_PREFIX = os.getenv("POSTIME_API_PREFIX", "/api")
+DTS_API_PREFIX = os.getenv("POSTIME_DTS_API_PREFIX", "/api/dts")
+
+SPECS = os.getenv("DTS_SPEC_URL",
+                  "https://distributed-text-services.github.io/specifications/context/1-alpha1.json")
+
+def filter_data(data, keys=None, keys_to_remove=None):
+    if keys and keys_to_remove:
+        raise ValueError("Both keys and keys_to_remove provided")
+    return {
+        key: val for key, val in data.items()
+        if (keys and key in keys)
+           or (keys_to_remove and key not in keys_to_remove)
+    }
+
+def get_id_from_name(name, prefix):
+    return name.removesuffix('.xml').replace(f"prefix_", "").replace('_', ' ')
+
+def parse_xml(url):
+    root = None
+
+    try:
+        response = requests.get(url)
+        root = ET.fromstring(response.text)
+    except requests.RequestException as e:
+        logging.error(f"Error fetching file {url}: {e}")
+        return None
+    except ET.ParseError as e:
+        logging.error(f"Error parsing file {url}: {e}")
+
+    pages = root.findall(".//{http://www.tei-c.org/ns/1.0}pb") if root else []
+
+    return {
+        'firstPage': pages[0].attrib['n'] if len(pages) else None,
+        'lastPage': pages[-1].attrib['n'] if len(pages) else None,
+        'text': response.text
+    }
+
+
+def load_source(user, repo):
+    results = []
+
+    gh_api_url = f"https://api.github.com/repos/{user}/{repo}-TEI/contents/"
+    response = requests.get(gh_api_url)
+    for elem in response.json():
+        if not elem['name'].endswith('xml'):
+            continue
+
+        xml_info = parse_xml(elem['download_url'])
+        if xml_info:
+            results.append({
+                'id': elem['name'],
+                'title': get_id_from_name(elem['name'], repo),
+                'info': parse_xml(elem['download_url'])
+            })
+
+    return results
+
+
+def load_data(path):
+    result = []
+
+    with open(path, "r") as inp_file:
+        data = json.load(inp_file)
+
+        for source in data:
+            result.append({
+                'id': source['id'],
+                'title': source['title'],
+                'description': source['description'],
+                'sermons': load_source('postime', source['id'].replace('_', ''))
+            })
+
+    return result
+
+data = load_data(DATA_PATH)
+data_index = {row['id']: row for row in data}
+
+app = Flask(__name__)
+cors = CORS(app, resources={
+    f"{API_PREFIX}/*": {"origins": API_CLIENT},
+    f"{DTS_API_PREFIX}/*": {"origins": "*"},
+})
+
+
+def format_response_dts(data):
+    headers = {
+        "@context": SPECS,
+        "dtsVersion": "1-alpha",
+        "@id": API_PREFIX,
+        "@type": "EntryPoint",
+        "collection": "/dts/api/collection/{?id,page,nav}",
+        "navigation" : "/dts/api/navigation/{?resource,ref,start,end,down,tree,page}",
+        "document": "/dts/api/document/{?resource,ref,start,end,tree,mediaType}"
+    }
+    return jsonify(data)
+
+
+@app.route(API_PREFIX)
+def index():
+    return jsonify([filter_data(source, keys=["id", "title", "description"]) for source in data])
+
+@app.route(f"{API_PREFIX}/<string:source_id>")
+def get_source(source_id):
+    if source_id not in data_index:
+        abort(404)
+    return jsonify(data_index[source_id])
+
+@app.route(f"{API_PREFIX}/<string:source_id>/<string:sermon_id>")
+def get_sermon(source_id, sermon_id):
+    if source_id not in data_index:
+        abort(404)
+
+    for sermon in data_index[source_id]['sermons']:
+        if sermon['id'] == sermon_id:
+            return jsonify(sermon)
+
+    abort(404)
+
+@app.route(f"{DTS_API_PREFIX}/collection")
+def get_collection():
+    return jsonify({})
+
+@app.route(f"{DTS_API_PREFIX}/navigation")
+def navigation():
+    return jsonify({})
+
+@app.route(f"{DTS_API_PREFIX}/document")
+def document():
+    return jsonify({})
+
+if __name__ == "__main__":
+    app.run(host=HOST, port=PORT)
